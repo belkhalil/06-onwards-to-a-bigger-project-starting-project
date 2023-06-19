ac5f912bd9118ce77a3e7762e60f951ff99d8b78
Abdeljabar AIT BELKHALIL <abdeljabar.aitbelkhalil@cgi.com>
2023-06-19 16:44:34 +0200

    add details

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+
+const MeetupDetailPage = () => {
+
+    return <MeetupDetails
+        image="https://pbs.twimg.com/media/DNVDOHOX4AAbJ8-?format=jpg&name=900x900"
+        title="a first meetup "
+        description="This is a description "
+
+    />
+}
+export default MeetupDetailPage;
